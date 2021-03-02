aa6883dee12047056d0095b4d05168aa395be4b5
Shinichiro Suzuki <shintrfc@gmail.com>
2021-03-02 18:59:30 +0900

    impliment navigation

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,23 +1,35 @@
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
-import { ColorSchemeName } from 'react-native';
-
-import NotFoundScreen from '../screens/NotFoundScreen';
-import { RootStackParamList } from '../types';
-import BottomTabNavigator from './BottomTabNavigator';
-import LinkingConfiguration from './LinkingConfiguration';
+import {
+	NavigationContainer,
+	DefaultTheme,
+	DarkTheme,
+} from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
+import { ColorSchemeName, View } from "react-native";
+import { MaterialCommunityIcons, Octicons } from "@expo/vector-icons";
+// import { View } from "../components/Themed";
+// import { Colors } from "react-native/Libraries/NewAppScreen";
+import Colors from "../constants/Colors";
+import NotFoundScreen from "../screens/NotFoundScreen";
+import { RootStackParamList } from "../types";
+import MainTabNavigator from "./MainTabNavigator";
+import LinkingConfiguration from "./LinkingConfiguration";
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
-export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
-  return (
-    <NavigationContainer
-      linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <RootNavigator />
-    </NavigationContainer>
-  );
+export default function Navigation({
+	colorScheme,
+}: {
+	colorScheme: ColorSchemeName;
+}) {
+	return (
+		<NavigationContainer
+			linking={LinkingConfiguration}
+			theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+		>
+			<RootNavigator />
+		</NavigationContainer>
+	);
 }
 
 // A root stack navigator is often used for displaying modals on top of all other content
@@ -25,10 +37,50 @@ export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeNa
 const Stack = createStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Root" component={BottomTabNavigator} />
-      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
-    </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator
+			screenOptions={{
+				headerStyle: {
+					backgroundColor: Colors.light.tint,
+					shadowOpacity: 0,
+					elevation: 0,
+				},
+				headerTintColor: Colors.light.background,
+				headerTitleAlign: "left",
+				headerTitleStyle: {
+					fontWeight: "bold",
+				},
+			}}
+		>
+			<Stack.Screen
+				name='Root'
+				component={MainTabNavigator}
+				options={{
+					title: "WhatsApp",
+					headerRight: () => (
+						<View
+							style={{
+								flexDirection: "row",
+								width: 60,
+								justifyContent: "space-between",
+								marginRight: 10,
+							}}
+						>
+							<Octicons name='search' size={22} color={"white"} />
+							<MaterialCommunityIcons
+								name='dots-vertical'
+								size={22}
+								color={"white"}
+							/>
+						</View>
+					),
+				}}
+			/>
+			<Stack.Screen
+				name='NotFound'
+				component={NotFoundScreen}
+				options={{ title: "Oops!" }}
+			/>
+		</Stack.Navigator>
+	);
 }
